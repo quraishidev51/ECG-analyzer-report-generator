d3dd2439a07fee11c8edc3a193a130e40ccc3114
quraishidev51 <quraishi.dev51@gmail.com>
2026-07-27 23:41:50 +0530

    Create utils.py for shared helper functions
    Added a new utils.py file with a module docstring.

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+"""
+utils.py
+
+Shared helper functions used across the ECG Analyzer project.
+
+Examples:
+- File handling
+- Signal utilities
+- Common calculations
+- Logging
+"""
+
+# Utility functions will be implemented here.
