3feac8507631a1db9725e80cd0de1edf9f46858f
quraishidev51 <quraishi.dev51@gmail.com>
2026-07-27 23:37:29 +0530

    Create model.py

---
+++ src/model.py
@@ -0,0 +1,13 @@
+"""
+model.py
+
+Machine learning models for ECG analysis.
+
+This module will contain:
+- Model training
+- Model loading
+- Prediction functions
+- Model evaluation
+"""
+
+# Machine learning code will be added here.
