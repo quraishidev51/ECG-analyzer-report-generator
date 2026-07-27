6c4df7bfc5409e986bcddc64d1328cb4b4948eaf
quraishidev51 <quraishi.dev51@gmail.com>
2026-07-27 23:34:20 +0530

    Create feature_extraction.py for ECG features
    Added a new file for ECG feature extraction functions.

---
+++ src/feature_extraction.py
@@ -0,0 +1,15 @@
+"""
+feature_extraction.py
+
+Functions for extracting features from ECG signals.
+
+Examples:
+- Heart rate
+- RR intervals
+- QRS duration
+- PR interval
+- QT interval
+- Wave amplitudes
+"""
+
+# Feature extraction functions will be added here.
