0fa3457b4f58b921770490be2763a0462b2dc233
quraishidev51 <quraishi.dev51@gmail.com>
2026-08-01 18:01:25 +0530

    Update predict_ecg function to accept model parameter

--- app/predictor.py
+++ app/predictor.py
@@ -1,5 +1,5 @@
 CLASS_NAMES = ["Conduction Disturbance","Hypertrophy","Myocardial Infarction","Normal","ST/T Change"]
-def predict_ecg(ecg_signal, threshold = 0.5):
+def predict_ecg(model, ecg_signal, threshold = 0.5):
       # Add batch dimension, example: X[0]5
     ecg_signal = np.expand_dims(ecg_signal, axis=0)
 
