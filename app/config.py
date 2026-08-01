656fbf967265383516c92a4f981abc529986cfa0
quraishidev51 <quraishi.dev51@gmail.com>
2026-08-01 23:15:10 +0530

    Add configuration for model path and class names

---
+++ app/config.py
@@ -0,0 +1,9 @@
+MODEL_PATH = "/content/drive/MyDrive/your_actual_path/best_model.keras"
+
+CLASS_NAMES = [
+    "Conduction Disturbance",
+    "Hypertrophy",
+    "Myocardial Infarction",
+    "Normal",
+    "ST/T Change"
+]
