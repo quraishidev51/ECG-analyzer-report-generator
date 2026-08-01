80d5b9998c87ed2ccc87df02e215ecdff0a36611
quraishidev51 <quraishi.dev51@gmail.com>
2026-08-01 17:50:30 +0530

    Implement ECG prediction function
    Added a function to predict output class with confidence levels.

--- app/predictor.py
+++ app/predictor.py
@@ -1,1 +1,25 @@
+CLASS_NAMES = ["Conduction Disturbance","Hypertrophy","Myocardial Infarction","Normal","ST/T Change"]
+def predict_ecg(ecg_signal, threshold = 0.5):
+      # Add batch dimension, example: X[0]5
+    ecg_signal = np.expand_dims(ecg_signal, axis=0)
 
+    probs = model.predict(ecg_signal, verbose=0)[0]
+
+    probabilities = {
+        cls: float(prob)
+        for cls, prob in zip(CLASS_NAMES, probs)
+    }
+
+    predictions = [
+        {
+            "label": cls,
+            "confidence" : float(prob)
+        }
+        for cls, prob in probabilities.items()
+        if prob >= threshold
+    ]
+
+    return {
+    "probabilities": probabilities,
+    "predictions": predictions
+}
