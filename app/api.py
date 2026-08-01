6439653f09407ad16cf100e3ac52f732f40d93a7
quraishidev51 <quraishi.dev51@gmail.com>
2026-08-02 00:03:35 +0530

    Initialize FastAPI with home route

--- app/api.py
+++ app/api.py
@@ -1,1 +1,12 @@
+from fastapi import FastAPI
 
+app = FastAPI(
+    title="ECG Analyzer API",
+    version="1.0.0"
+)
+
+@app.get("/")
+def home():
+    return {
+        "message": "ECG Analyzer API is running!"
+    }
